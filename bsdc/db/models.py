49767bb7bd6da95e59ec9e3d6780dea839c31c0e
ann <ann@adimian.com>
2022-10-27 09:39:13 +0200

    initial commit

--- bsdc/db/models.py
+++ bsdc/db/models.py
@@ -3,19 +3,48 @@ from sqlalchemy.orm import declarative_base
 
 _Base = declarative_base()
 
-class Base(_Base): # IGNORE
+
+class Base(_Base):  # IGNORE
     __abstract__ = True
-    
+
+
 class Participant(Base):
     __tablename__ = "participant"
-    
+
     id = Column(Integer, primary_key=True)
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
-    type_of_participant = Column(Enum(types))
-    
+    type_of_participant = Column(Enum(Types))
+    admin_rights = Column(Boolean, default=False)
+
     class Types(enum.Enum):
         regular = 1
         student = 2
         volunteer = 3
         teacher = 4
+
+
+class Courses(Base):
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    topic = Column(Enum(Topics))
+    level = Column(Enum(Levels))
+
+    class Topics(enum.Enum):
+        lindy_hop = 1
+        jazz_solo = 2
+        mix_jazz_lindy = 3
+        technique = 4
+        rhythm = 5
+        balboa = 6
+        shag = 7
+        blues = 8
+        show = 9
+
+    class Levels(enum.Enum):
+        beginner = 1
+        beginner_intermediate = 2
+        intermediate = 3
+        intermeciate_advanced = 4
+        advanced = 5
+        mixed = 6
--- bsdc/main.py
+++ bsdc/main.py
@@ -5,3 +5,21 @@ from hypercorn.asyncio import serve
 from hypercorn.config import Config
 from pydantic import BaseSettings
 
+from bsdc.db import get_session, get_engine
+from bsdc.db.models import Base
+from bsdc.routers.v1 import root
+
+class Settings(BaseSettings):
+    port: int = 2833
+    
+def app_maker(testing: bool = False):
+    app = FastAPI(version="1.0.0")
+    app.include_router(root, prefix="/api")
+    if not testing:
+        
+        engine = get_engine()
+        Base.metadata.bind = engine
+        Base.metadata.create_all()
+        session = next(get_session(engine=engine))
+        
+    return app
